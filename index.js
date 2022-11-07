bdfd117199d0ee9c3eefa8c4954b48ef14a4eaef
corbin <cpnbreeze@gmail.com>
2022-11-06 18:16:35 -0800

    commit tree 223a87e237e343bb2e76ed7d69be102ae4e04449

---
+++ index.js
@@ -0,0 +1,18 @@
+function add() {
+    return "something";
+}
+function add (a, b) {
+    return a + b;
+}
+function subtract(){
+    return "something";
+}
+function subtract (a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a/ b;
+}
